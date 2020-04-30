9f1765c98def407bbbf996e220514d111d074eee
Brooke Luro <lurob@me.com>
2020-04-30 14:38:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(element) {
-  var myArray = [1, 2, 3] 
+  var myArray = [1, 2, 3]
   return myArray.unshift(element)
 }
-
-
