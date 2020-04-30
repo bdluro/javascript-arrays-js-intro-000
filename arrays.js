9752f02456627e32085aa7c18cf4715a308d782a
Brooke Luro <lurob@me.com>
2020-04-30 14:57:42 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,7 +4,13 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
 
-function destructivelyAddElementToBeginningOfArray (array, element) {
+function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
 }
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+
